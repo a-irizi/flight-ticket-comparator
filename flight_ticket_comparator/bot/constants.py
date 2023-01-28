c35a1b203d547c8b634c894a9dbbb4bfa4ea4c68
a-irizi <irizi.anass.00@gmail.com>
2023-01-28 18:40:16 +0100

    Add file for storing constants values used by bots

---
+++ flight_ticket_comparator/bot/constants.py
@@ -0,0 +1,7 @@
+import os
+
+DRIVER_PATH = os.path.join(os.environ['HOME'], 'Downloads')
+
+ROYAL_AIR_MAROC_URL = "https://www.royalairmaroc.com/us-en"
+RYANAIR_URL = "https://www.ryanair.com/us/en"
+QATAR_AIRWAYS_URL = "https://www.qatarairways.com/en-us/homepage.html"
